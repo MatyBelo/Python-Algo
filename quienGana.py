0c95d57b0a51775e63e9a1140d5570791976c93e
Matías <133241458+MatyBelo@users.noreply.github.com>
2023-05-29 00:52:25 -0300

    Update quienGana.py

--- quienGana.py
+++ quienGana.py
@@ -1,12 +1,12 @@
 import sys
 
 def quienGana(j1: str, j2: str) -> str :
-    if (j1 == j2):
-        return ("Empate")
-    elif (((j1 == "Piedra") and (j2 == "Tijera")) or ((j1 == "Tijera") and (j2 == "Papel")) or ((j1 == "Papel") and (j2 == "Piedra"))):
-        return (j1)
-    elif (quienGana(j2,j1)):
-        return (j2)
+    if j1 == j2:
+        return "Empate"
+    elif (j1 == "Piedra" and j2 == "Tijera") or (j1 == "Tijera" and j2 == "Papel") or (j1 == "Papel" and j2 == "Piedra"):
+        return "Jugador1"
+    elif (j2 == "Piedra" and j1 == "Tijera") or (j2 == "Tijera" and j1 == "Papel") or (j2 == "Papel" and j1 == "Piedra"):
+        return "Jugador2"
 
 if __name__ == '__main__':
   x = input()
